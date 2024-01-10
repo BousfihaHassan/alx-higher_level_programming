1e0f942ea344dfc14754592b29376efc5d52a0e1
BousfihaHassan <Bousfihahassan9@gmail.com>
2024-01-10 10:36:13 -0800

    10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@ def best_score(a_dictionary):
     maxv = 0
     maxk = None
     for k, v in a_dictionary.items():
-        if v > maxval:
+        if v > maxv:
             maxv = v
             maxk = k
     return maxk
