c934999503abc4ad5d36ee7717ce18df15b9a7c9
bousfihahassan <bousfihahassan2019@gmail.com>
2024-04-08 18:07:45 -0700

    js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
+
 // Script that prints C is fun,Python is cool and JavaScript is amazing.
 
-const cMessage = "C is fun";
-const pythonMessage = "Python is cool";
-const jsMessage = "JavaScript is amazing";
-console.log(cMessage);
-console.log(pythonMessage);
-console.log(jsMessage);
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(myVar);
